7b497d8a63b18d018805edc1317ccb4bf51e5eef
sungmin69355 <sungmin69355@naver.com>
2023-10-08 14:50:15 +0900

    상품 등록 Test => Spring Boot Test 전환

--- src/test/java/com/example/productorderservice/product/ProductPort.java
+++ src/test/java/com/example/productorderservice/product/ProductPort.java
@@ -1,5 +1,8 @@
 package com.example.productorderservice.product;
 
+import org.springframework.stereotype.Component;
+
+@Component
 interface ProductPort {
     void save(final Product product);
 }
--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -2,21 +2,16 @@ package com.example.productorderservice.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 
+@SpringBootTest
 class ProductServiceTest {
 
+    @Autowired
     private ProductService productService;
-    private ProductPort productPort;
-    private ProductRepository productRepository;
-
-    @BeforeEach
-    void setUp() {
-        productRepository  = new ProductRepository();
-        productPort = new ProductAdapter(productRepository);
-        productService = new ProductService(productPort);
-    }
 
     @Test
     void 상품등록() {
